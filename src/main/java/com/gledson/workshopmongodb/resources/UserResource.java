bb71295ad8e89f72c14e57cffbf95201cb532fd8
Gledson Oliveira <gleds3000@gmail.com>
2020-11-03 23:55:20 -0300

    first commit

---
+++ src/main/java/com/gledson/workshopmongodb/resources/UserResource.java
@@ -0,0 +1,31 @@
+package com.gledson.workshopmongodb.resources;
+
+
+import com.gledson.workshopmongodb.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/users")
+
+public class UserResource {
+
+    @RequestMapping(method= RequestMethod.GET)
+    public ResponseEntity <List<User>> findAll(){
+
+        User maria = new User("1", "Maria brown", "maria@teste.com");
+        User alex = new User("2","Alex", "alex@teste.com");
+        List<User> list = new ArrayList<>();
+
+        list.addAll(Arrays.asList(maria, alex));
+        return ResponseEntity.ok().body(list);
+    }
+
+}
