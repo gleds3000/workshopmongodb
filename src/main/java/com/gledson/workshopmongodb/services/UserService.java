90e5af47c90d4f08e04771cc8afd961b1eb42a11
Gledson Oliveira <gleds3000@gmail.com>
2020-11-07 00:59:23 -0300

    obtendo usuario por id e seus devidos tratamentos quando nao encontrado.

--- src/main/java/com/gledson/workshopmongodb/services/UserService.java
+++ src/main/java/com/gledson/workshopmongodb/services/UserService.java
@@ -2,10 +2,12 @@ package com.gledson.workshopmongodb.services;
 
 import com.gledson.workshopmongodb.domain.User;
 import com.gledson.workshopmongodb.repository.UserRepository;
+import com.gledson.workshopmongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -17,6 +19,11 @@ public class UserService {
     public List<User> findAll(){
         return repo.findAll();
 
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
 }
