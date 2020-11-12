04b658e85360b457683cb38d459f0ccab8649418
Gledson Oliveira <gleds3000@gmail.com>
2020-11-11 23:42:52 -0300

    juncao do servico de posts com usuarios AuthorDTO

--- src/main/java/com/gledson/workshopmongodb/config/Instantiation.java
+++ src/main/java/com/gledson/workshopmongodb/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.gledson.workshopmongodb.config;
 
 import com.gledson.workshopmongodb.domain.Post;
 import com.gledson.workshopmongodb.domain.User;
+import com.gledson.workshopmongodb.dto.AuthorDTO;
 import com.gledson.workshopmongodb.repository.PostRepository;
 import com.gledson.workshopmongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,10 +35,11 @@ public class Instantiation implements CommandLineRunner {
         User gledson = new User(null, "Gledson", "gledson@gmail.com");
         User luiz = new User(null, "luiz", "luiz@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2020"), "Partiu viagem", "Vou viajar para Sao Paulo!", gledson);
-        Post post2 = new Post(null, sdf.parse("23/03/2020"), "Bom dia", "Vou ser feliz hoje!", gledson);
-
         userRepository.saveAll(Arrays.asList(bere,gledson,luiz));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2020"), "Partiu viagem", "Vou viajar para Sao Paulo!", new AuthorDTO(gledson));
+        Post post2 = new Post(null, sdf.parse("23/03/2020"), "Bom dia", "Vou ser feliz hoje!", new AuthorDTO(gledson));
+
         postRepository.saveAll(Arrays.asList(post1,post2));
     }
 }
--- src/main/java/com/gledson/workshopmongodb/domain/Post.java
+++ src/main/java/com/gledson/workshopmongodb/domain/Post.java
@@ -1,5 +1,6 @@
 package com.gledson.workshopmongodb.domain;
 
+import com.gledson.workshopmongodb.dto.AuthorDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -15,12 +16,12 @@ public class Post implements Serializable {
     private Date date;
     private String title;
     private String body;
-    private User author;
+    private AuthorDTO author;
 
     public Post() {
     }
 
-    public Post(String id, Date date, String title, String body, User author) {
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
         this.id = id;
         this.date = date;
         this.title = title;
@@ -60,6 +61,15 @@ public class Post implements Serializable {
         this.body = body;
     }
 
+
+    public AuthorDTO getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(AuthorDTO author) {
+        this.author = author;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -75,11 +85,4 @@ public class Post implements Serializable {
         return getId().hashCode();
     }
 
-    public User getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(User author) {
-        this.author = author;
-    }
 }
